2d55b664dc5b16314b257a220be409d8f8112c3a
Adrian Kim <uknoiluv@gmail.com>
2014-10-16 18:29:32 -0700

    10/16_num_separated_by_comma_added

--- calculator.js
+++ calculator.js
@@ -11,17 +11,47 @@
     var opCount = 0;
     var lastOp = '';
 
+    var clearSelected = function(){
+      if($('.selected')){
+        $('.selected').css('height', '100px');
+        $('.selected').css('width', '100px');
+        $('.selected').css('border', 'solid 1px black');
+        $('.selected').removeClass('selected');
+      }
+    };
+
+    var commaSeperator = function(str){
+      var strArr = (str + '').split('');
+      var strArrLen = strArr.length;
+      if(strArrLen > 9){
+        if((str[0] + '.' + str.slice(1,7)) * 1 === 9.999999){
+          return '1e+' + strArrLen;
+        }else{
+          var pow6 = Math.pow(10, 6);
+          return Math.round((str[0] + '.' + str.slice(1,8)) * pow6)/pow6  + 'e+' + (strArrLen - 1);
+        }
+      }else{
+        var k = strArrLen - 3;
+        while(k > 0){
+          strArr.splice(k, 0, ',');
+          k -= 3;
+        }
+        return strArr.join('');
+      }
+    };
+
     var detectNum = function(num){
       return $('#' + num).on('click', function(){
         if(stringToBeEval.length && !isNaN(stringToBeEval[stringToBeEval.length - 1])){
           numString = '';
         }
-        numString += $(this).html();
+        numString.length < 9 ? numString += $(this).html() : null ;
         numString = numString * 1 + '';
-        $('.row1').html(numString);
+        $('.row1').html(commaSeperator(numString));
         if($('#clear').html() === 'AC'){
           $('#clear').html('C');
         }
+        clearSelected();
       });
     };
 
@@ -47,45 +77,40 @@
           numString = '';
         }
         if(opCount > 1){
-          $('.row1').html(eval(stringToBeEval.slice(0, -1)));
+          $('.row1').html(commaSeperator(eval(stringToBeEval.slice(0, -1))));
         }
-        if($('.selected')){
-          $('.selected').css('height', '100px');
-          $('.selected').css('width', '100px');
-          $('.selected').css('border', 'solid 1px black');
-          $('.selected').removeClass('selected');
-        }
+        clearSelected();
         $(this).css('height', '96px');
         $(this).css('width', '96px');
         $(this).css('border', 'solid 3px black');
         $(this).addClass('selected');
       });
     };
-
-
+    
     $('.row1').html(numString);
 
     $('#equals').on('click', function(){
       numString === '' ? stringToBeEval += $('.row1').html() : stringToBeEval += numString;
       console.log('stringToBeEval', stringToBeEval);
-      $('.row1').html(eval(stringToBeEval));
+      $('.row1').html(commaSeperator(eval(stringToBeEval) + ''));
       console.log('lastOp', lastOp);
       stringToBeEval = eval(stringToBeEval) + lastOp;
       opCount = 0;
+      clearSelected();
     });
 
     $('#percent').on('click', function(){
       if(+numString > 0){
         numString = numString / 100 + '';
       }
-      $('.row1').html(numString);
+      $('.row1').html(commaSeperator(numString));
     });
 
     $('#plusMinus').on('click', function(){
       if(+numString !== 0){
         numString = -numString;
       }
-      $('.row1').html(numString);
+      $('.row1').html(commaSeperator(numString));
     });
 
     $('#clear').on('click', function(){
@@ -93,7 +118,8 @@
         stringToBeEval = '';
         numString = '0';
         opCount = 0;
-        $('.row1').html(numString);
+        $('.row1').html(commaSeperator(numString));
+        clearSelected();
       }else{
         numString = '0';
         $('.row1').html(0);
@@ -109,7 +135,7 @@
       if(numString.indexOf('.') === -1){
         numString *= 1;
         numString += $(this).html();
-        $('.row1').html(numString);
+        $('.row1').html(commaSeperator(numString));
       }
     });
 
